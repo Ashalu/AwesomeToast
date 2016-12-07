184260739dff77765b80471bca407a331ef86e14
Ashish Kumar <akbrother02@gmail.com>
2016-12-07 13:25:26 +0530

    Getting Classes

--- app/src/main/java/loadingdata/ashish/awesometoast/MainActivity.java
+++ app/src/main/java/loadingdata/ashish/awesometoast/MainActivity.java
@@ -1,32 +1,50 @@
 package loadingdata.ashish.awesometoast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-    CardView Info,Download,Error,Confusion,Warning,Default;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
-        Widget_Initialisation();
-        setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle("Awesome Toast");
-        }
     }
 
-    private void Widget_Initialisation() {
-        Info=(CardView)findViewById(R.id.information);
-        Download=(CardView)findViewById(R.id.download);
-        Error=(CardView)findViewById(R.id.error);
-        Confusion=(CardView)findViewById(R.id.confusion);
-        Warning=(CardView)findViewById(R.id.warning);
-        Default=(CardView)findViewById(R.id.def);
-        toolbar=(Toolbar)findViewById(R.id.my_toolbar);
+    public void showSuccessToast(View view) {
+        TastyToast.makeText(getApplicationContext(), "Download Successful !", TastyToast.LENGTH_LONG,
+                TastyToast.SUCCESS);
+    }
+
+    public void showWarningToast(View view) {
+        TastyToast.makeText(getApplicationContext(), "Are you sure ?", TastyToast.LENGTH_LONG,
+                TastyToast.WARNING);
+    }
+
+    public void showErrorToast(View view) {
+        TastyToast.makeText(getApplicationContext(), "Downloading failed ! Try again later ", TastyToast.LENGTH_LONG,
+                TastyToast.ERROR);
+    }
+    public void showInfoToast(View view) {
+        TastyToast.makeText(getApplicationContext(), "Searching for username : 'Rahul' ", TastyToast.LENGTH_LONG,
+                TastyToast.INFO);
+    }
+
+    public void showDefaultToast(View view) {
+        TastyToast.makeText(getApplicationContext(), "This is Default Toast", TastyToast.LENGTH_LONG,
+                TastyToast.DEFAULT);
+    }
+
+
+    public void showConfusingToast(View view) {
+        TastyToast.makeText(getApplicationContext(), "I don't Know !", TastyToast.LENGTH_LONG,
+                TastyToast.CONFUSING);
     }
 }
